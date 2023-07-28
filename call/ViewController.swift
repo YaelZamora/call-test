99cddddf2e67ce1393a34e7d1ae51ab7ab534fa8
YaelZamora <ajedrezkasparov@gmail.com>
2023-07-28 10:40:44 -0600

    Agora implement

--- call/ViewController.swift
+++ call/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import AgoraUIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let agview = AgoraVideoViewer(connectionData: AgoraConnectionData(appId: "eaa38390cf1e4cd1abe91ee18a453250"))
+        agview.fills(view: self.view)
+        agview.join(channel: "test", as: .broadcaster)
     }
 
 
