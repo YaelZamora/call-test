7d1fe0474f429869445f8544bdb4f47be48d126b
YaelZamora <ajedrezkasparov@gmail.com>
2023-07-28 11:35:47 -0600

    prettier code

--- call/ViewController.swift
+++ call/ViewController.swift
@@ -10,14 +10,44 @@ import AgoraUIKit
 
 class ViewController: UIViewController {
 
+    // Fill the App ID of your project generated on Agora Console.
+    let appId: String = "eaa38390cf1e4cd1abe91ee18a453250"
+
+    // Fill the temp token generated on Agora Console.
+    let token: String? = "007eJxTYFhe3fnJIelCOOtDs0rh3OdL3tp6NfzNrfK0S8zO9TiYsVSBITUx0djC2NIgOc0w1SQ5xTAxKdXSMDXV0CLRxNTYyNSA6/fhlIZARobilfpMjAwQCOKzMJSkFpcwMAAAcu8f7A=="
+
+    // Fill the channel name.
+    let channelName: String = "test"
+
+    // Create the view object.
+    var agoraView: AgoraVideoViewer!
+    
+    func initializeAndJoinChannel(){
+
+        agoraView = AgoraVideoViewer(
+            connectionData: AgoraConnectionData(
+                appId: appId,
+                rtcToken: token
+            )
+        )
+        agoraView.fills(view: self.view)
+
+        agoraView.join(
+            channel: channelName,
+            with: token,
+            as: .broadcaster
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let agview = AgoraVideoViewer(connectionData: AgoraConnectionData(appId: "eaa38390cf1e4cd1abe91ee18a453250"))
-        agview.fills(view: self.view)
-        agview.join(channel: "test", as: .broadcaster)
+        initializeAndJoinChannel()
     }
 
-
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        agoraView.exit()
+    }
 }
 
