0d1cd8d1b56f5f109c7a552148dcf054aca90922
YaelZamora <ajedrezkasparov@gmail.com>
2023-08-01 13:52:55 -0600

    Agora new token

--- call/ViewController.swift
+++ call/ViewController.swift
@@ -9,45 +9,17 @@ import UIKit
 import AgoraUIKit
 
 class ViewController: UIViewController {
-
-    // Fill the App ID of your project generated on Agora Console.
-    let appId: String = "eaa38390cf1e4cd1abe91ee18a453250"
-
-    // Fill the temp token generated on Agora Console.
-    let token: String? = "007eJxTYFhe3fnJIelCOOtDs0rh3OdL3tp6NfzNrfK0S8zO9TiYsVSBITUx0djC2NIgOc0w1SQ5xTAxKdXSMDXV0CLRxNTYyNSA6/fhlIZARobilfpMjAwQCOKzMJSkFpcwMAAAcu8f7A=="
-
-    // Fill the channel name.
-    let channelName: String = "test"
-
-    // Create the view object.
-    var agoraView: AgoraVideoViewer!
-    
-    func initializeAndJoinChannel(){
-
-        agoraView = AgoraVideoViewer(
-            connectionData: AgoraConnectionData(
-                appId: appId,
-                rtcToken: token
-            )
-        )
-        agoraView.fills(view: self.view)
-
-        agoraView.join(
-            channel: channelName,
-            with: token,
-            as: .broadcaster
-        )
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        initializeAndJoinChannel()
+        let agview = AgoraVideoViewer(connectionData: AgoraConnectionData(appId: "eaa38390cf1e4cd1abe91ee18a453250", rtcToken: "007eJxTYKiY9OSujsCLaZt37GISfXr+Y4uHwpnmvV4Xd1S+7746w/OJAkNqYqKxhbGlQXKaYapJcophYlKqpWFqqqFFoompsZGpgbTLyZSGQEaG5lBBBkYoBPG5GXwyy1KdMxLz8lJzGBgAPtskgg=="))
+        agview.fills(view: self.view)
+        agview.join(channel: "LiveChannel", as: .broadcaster)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        agoraView.exit()
     }
 }
 
+//cdcab36a6cbf4ca9b2bc959bd1799540
