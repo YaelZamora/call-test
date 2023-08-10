6ed4bfc434385110e7a8b1ccd5b68ac7bd6d9db9
YaelZamora <ajedrezkasparov@gmail.com>
2023-08-09 18:52:42 -0600

    zoom

--- call/ViewController.swift
+++ call/ViewController.swift
@@ -10,11 +10,22 @@ import AgoraUIKit
 
 class ViewController: UIViewController {
     
+    var zoomBtn: UIButton!
+    var isZoomed: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let agview = AgoraVideoViewer(connectionData: AgoraConnectionData(appId: "eaa38390cf1e4cd1abe91ee18a453250", rtcToken: "007eJxTYKiY9OSujsCLaZt37GISfXr+Y4uHwpnmvV4Xd1S+7746w/OJAkNqYqKxhbGlQXKaYapJcophYlKqpWFqqqFFoompsZGpgbTLyZSGQEaG5lBBBkYoBPG5GXwyy1KdMxLz8lJzGBgAPtskgg=="))
+        let agview = AgoraVideoViewer(connectionData: AgoraConnectionData(appId: "eaa38390cf1e4cd1abe91ee18a453250", rtcToken: "007eJxTYNjSrLyuuEbZ597LEA/Jy/N3cQh8OChcYX1aPW2pws4z4SkKDKmJicYWxpYGyWmGqSbJKYaJSamWhqmphhaJJqbGRqYGs1aeSmkIZGQ4uGAtCyMDBIL43Aw+mWWpzhmJeXmpOQwMAD2AIsw="))
+        
         agview.fills(view: self.view)
         agview.join(channel: "LiveChannel", as: .broadcaster)
+        
+        zoomBtn = UIButton(type: .system)
+        zoomBtn.frame = CGRect(x: 120, y: 650, width: 180, height: 50)
+        zoomBtn.setTitle("Zoom In", for: .normal)
+        
+        //zoomBtn.addTarget(self, action: #selector(zoomBtnClicked), for: .touchUpInside)
+        self.view.addSubview(zoomBtn)
     }
 
     override func viewDidDisappear(_ animated: Bool) {
